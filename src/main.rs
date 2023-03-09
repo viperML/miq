43bc5764e3bebcdc77db5fe0c67fdad9f79a6921
Fernando Ayats <ayatsfer@gmail.com>
2023-03-09 20:37:39 +0100

    feat: add logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    setup_logging()?;
+
+    log::info!("Hello World");
+
+    Ok(())
+}
+
+fn setup_logging() -> anyhow::Result<()> {
+    let loglevel = log::LevelFilter::Debug;
+
+    fern::Dispatch::new()
+        .format(|out, message, record| out.finish(format_args!("[{}] {}", record.level(), message)))
+        .level(loglevel)
+        .chain(std::io::stdout())
+        .apply()?;
+
+    Ok(())
 }
