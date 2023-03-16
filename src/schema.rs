3d19965b61863e3ea8a98e1dd5031cbba5695ce6
Fernando Ayats <ayatsfer@gmail.com>
2023-03-16 10:50:45 +0100

    feat: add schema doc

--- src/schema.rs
+++ src/schema.rs
@@ -11,23 +11,34 @@ use serde::Deserialize;
  rm ~/.config/Code/User/globalStorage/tamasfe.even-better-toml/*
 */ */
 
+/// Definition of a package. A package the minimum buildable unit
 #[derive(JsonSchema, Debug, Deserialize)]
 pub struct Pkg {
+    /// Name of the package, normalized
     pub name: String,
+    /// Version of the package, normalized
     pub version: String,
+    /// List of fecthables available during build-time
     pub fetch: Vec<Fetchable>,
+    /// POSIX script excuted at build-time
     pub script: String,
+    /// Path that this package produces
     pub path: String,
 }
 
+/// A fetchable is fetched from the internet and hash-checked
 #[derive(JsonSchema, Debug, Deserialize)]
 pub struct Fetchable {
+    /// URL to fetch
     pub url: String,
+    /// SRI hash to check for integrity
     pub hash: String,
 }
 
+/// miq consumes pkg-spec files
 #[derive(JsonSchema, Debug, Deserialize)]
 pub struct PkgSpec {
+    /// List of packages to build
     pub pkg: Vec<Pkg>,
 }
 
