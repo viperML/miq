716aff58e5bb9a71e298cfe9bef3738382a01d8a
Fernando Ayats <ayatsfer@gmail.com>
2023-05-02 12:12:10 +0200

    lua mk_package

--- src/lua.rs
+++ src/lua.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
 use std::ptr::hash;
 
@@ -8,10 +9,10 @@ use mlua::serde::de;
 use serde::{Deserialize, Serialize};
 use sha2::digest::Update;
 use sha2::{Digest, Sha256};
-use tracing::{debug, info};
+use tracing::{debug, info, trace};
 use url::Url;
 
-use crate::schema_eval::{Fetch, Unit};
+use crate::schema_eval::{Fetch, Package, Unit};
 
 #[derive(Debug, clap::Args)]
 pub struct Args {}
@@ -38,41 +39,55 @@ impl Args {
         let lua_mk_fetch = lua.create_function(|ctx, input: LuaValue| {
             // -
             let deser = ctx.from_value::<FetchInput>(input)?;
-            let deser = compute_unit(deser);
+            let deser = compute_fetch(deser);
             let deser = deser.expect("FATAL");
 
-            info!(?deser);
+            trace!(?deser);
+
+            Ok(deser)
+        })?;
+
+        let lua_mk_package = lua.create_function(|ctx, input: LuaValue| {
+            // -
+            let deser = ctx.from_value::<PackageInput>(input)?;
+            let deser = compute_package(deser);
+            let deser = deser.expect("FATAL");
+
+            trace!(?deser);
 
             Ok(deser)
         })?;
 
         lua.load(chunk! {
             mqf = {
-                mk_fetch = $lua_mk_fetch
+                mk_fetch = $lua_mk_fetch,
+                mk_package = $lua_mk_package,
             }
         })
         .exec()?;
 
         lua.load(chunk! {
             // pkgs.lua
-            pkg_test = mqf.mk_fetch({
-                url = "https://wdtz.org/files/gywxhjgl70sxippa0pxs0vj5qcgz1wi8-stdenv-bootstrap-tools/on-server/bootstrap-tools.tar.xz"
-            })
+            local mk_fetch = mqf.mk_fetch
+            local mk_package = mqf.mk_package
 
-            busybox = mqf.mk_fetch({
+            pkg_test = mk_fetch {
+                url = "https://wdtz.org/files/gywxhjgl70sxippa0pxs0vj5qcgz1wi8-stdenv-bootstrap-tools/on-server/bootstrap-tools.tar.xz"
+            }
+
+            busybox = mk_fetch {
                 url = "https://wdtz.org/files/gywxhjgl70sxippa0pxs0vj5qcgz1wi8-stdenv-bootstrap-tools/on-server/busybox",
                 executable = true,
-            })
+            }
+
+            bootstrap = mk_package {
+                name = "bootstrap",
+                version = "0.1.0",
+            }
 
             print(pkg_test)
         })
         .exec()?;
-
-        let x: Unit = globals.get("pkg_test")?;
-        info!(?x);
-
-        let test_result: LuaValue = globals.get("pkg_test")?;
-        info!(?test_result);
 
         Ok(())
     }
@@ -84,6 +99,14 @@ struct FetchInput {
     executable: Option<bool>,
 }
 
+#[derive(Debug, Serialize, Deserialize, Hash)]
+struct PackageInput {
+    name: String,
+    version: Option<String>,
+    deps: Option<Vec<String>>,
+    script: Option<String>,
+}
+
 fn hash_string<H: Hash>(input: &H) -> String {
     let mut hasher = fnv::FnvHasher::default();
     input.hash(&mut hasher);
@@ -92,11 +115,11 @@ fn hash_string<H: Hash>(input: &H) -> String {
     s
 }
 
-fn compute_fetch(input: FetchInput) -> Result<Fetch> {
+fn compute_fetch(input: FetchInput) -> Result<Unit> {
     let name = input
-    .url
-    .path_segments()
-    .expect("URL doesn't have segments")
+        .url
+        .path_segments()
+        .expect("URL doesn't have segments")
         .last()
         .unwrap()
         .to_owned();
@@ -104,7 +127,7 @@ fn compute_fetch(input: FetchInput) -> Result<Fetch> {
     let hash = hash_string(&input);
     let result = format!("{}-{}", name, hash);
     let path = format!("/miq/eval/{}.toml", result);
-    debug!(?path);
+    trace!(?path);
 
     let result = Fetch {
         result,
@@ -115,14 +138,30 @@ fn compute_fetch(input: FetchInput) -> Result<Fetch> {
     };
 
     let serialized = toml::to_string_pretty(&result)?;
-
     std::fs::write(path, serialized)?;
 
-    Ok(result)
+    Ok(Unit::FetchUnit(result))
 }
 
-fn compute_unit(input: FetchInput) -> Result<Unit> {
-    Ok(Unit::FetchUnit(compute_fetch(input)?))
+fn compute_package(input: PackageInput) -> Result<Unit> {
+    let hash = hash_string(&input);
+    let result = format!("{}-{}", input.name, hash);
+    let path = format!("/miq/eval/{}.toml", result);
+    trace!(?path);
+
+    let result = Package {
+        result,
+        name: input.name,
+        version: input.version.unwrap_or_default(),
+        deps: input.deps.unwrap_or_default(),
+        script: input.script.unwrap_or_default(),
+        env: HashMap::new(),
+    };
+
+    let serialized = toml::to_string_pretty(&result)?;
+    std::fs::write(path, serialized)?;
+
+    Ok(Unit::PackageUnit(result))
 }
 
 impl LuaUserData for Unit {}
