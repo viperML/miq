4125a02e52645d5417b94d439a704b676e5adbab
Fernando Ayats <ayatsfer@gmail.com>
2023-06-13 22:14:04 +0200

    add more pkgs

--- pkgs/stage1.lua
+++ pkgs/stage1.lua
@@ -84,7 +84,7 @@ stage1.stdenv = function(input)
 	input.env["CC"] = "gcc"
 	input.env["CXX"] = "g++"
 	input.env["LD"] = "ld"
-  input.env["CFLAGS"] = "-O2 -pipe -pie -fPIE -fPIC"
+	input.env["CFLAGS"] = "-O2 -pipe -pie -fPIE -fPIC"
 
 	return miq.package(input)
 end
@@ -107,30 +107,101 @@ stage1.trivial = stage1.stdenv {
 }
 
 local dash_src = fetch {
-  url = "http://gondor.apana.org.au/~herbert/dash/files/dash-0.5.12.tar.gz"
+	url = "http://gondor.apana.org.au/~herbert/dash/files/dash-0.5.12.tar.gz",
 }
 
 stage1.dash_src = stage1.stdenv {
-  name = "dash_src",
-  script = f [[
+	name = "dash_src",
+	script = f [[
     set -eux
     mkdir -pv $miq_out
     cd $miq_out
 
     tar -xvf {{dash_src}} --strip-components=1 --no-same-permissions --no-same-owner
-  ]]
+  ]],
 }
 
 stage1.dash = stage1.stdenv {
-  name = "dash",
-  script = f[[
+	name = "dash",
+	script = f [[
     {{stage1.dash_src}}/configure --prefix=$miq_out
     ls -la
 
     make -j$(nproc)
     make install -j$(nproc)
-  ]]
+  ]],
 }
 
+local m4 = {}
+m4.version = "1.4.19"
+m4.src = fetch {
+	url = f "https://ftp.gnu.org/gnu/m4/m4-{{m4.version}}.tar.bz2",
+}
+m4.pkg = stage1.stdenv {
+	name = "m4",
+	version = m4.version,
+	script = f [[
+    tar -xvf {{m4.src}} --strip-components=1 --no-same-permissions --no-same-owner
+    mkdir $miq_out
+    ./configure --prefix=$miq_out --with-syscmd-shell={{bootstrap.bootstrap}}
+    make -j$(nproc)
+    make install -j$(nproc)
+  ]],
+}
+stage1.m4 = m4
+
+local gmp = {}
+gmp.version = "6.2.1"
+gmp.src = fetch {
+	url = f "https://ftp.gnu.org/gnu/gmp/gmp-{{gmp.version}}.tar.bz2",
+}
+gmp.pkg = stage1.stdenv {
+	name = "gmp",
+	version = gmp.version,
+	script = f [[
+    export PATH="{{m4.pkg}}/bin:$PATH"
+    tar -xvf {{gmp.src}} --strip-components=1 --no-same-permissions --no-same-owner
+    mkdir $miq_out
+    ./configure --prefix=$miq_out --with-pic --with-cxx
+    make -j$(nproc)
+    make install -j$(nproc)
+  ]],
+}
+stage1.gmp = gmp
+
+local gcc_version = "12.2.0"
+
+local gcc_src_raw = fetch {
+	url = f "https://mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-{{gcc_version}}/gcc-{{gcc_version}}.tar.xz",
+}
+
+stage1.gcc_src = stage1.stdenv {
+	name = "gcc_src",
+	version = gcc_version,
+	script = f [[
+    mkdir $miq_out
+    cd $miq_out
+
+    tar -xvf {{gcc_src_raw}} --strip-components=1 --no-same-permissions --no-same-owner
+  ]],
+}
+
+stage1.gcc = stage1.stdenv {
+	name = "gcc",
+	version = gcc_version,
+	script = f [[
+    export PREFIX=$miq_out
+    mkdir $miq_out
+    cd $miq_out
+
+    {{stage1.gcc_src}}/configure \
+      --prefix="$PREFIX" \
+      --disable-nls \
+      --enable-languages=c,c++
+
+    make -j$(nproc)
+    make install -j$(nproc)
+  ]],
+}
 
 return stage1
